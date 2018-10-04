7bef52faa3ffcb06c5f1a89dead036729817333c
UAB\wwarr <wwarr@uab.edu>
2018-10-04 16:39:56 -0500

    added demo for hpc batch, i.e. sbatch array mode

---
+++ examples/hpc_batch_demo/hpc_batch_demo.m
@@ -0,0 +1,24 @@
+function hpc_batch_demo( stl_path, output_path )
+%% RESOURCES
+this_path = fileparts( mfilename( 'fullpath' ) );
+res_path = fullfile( this_path, 'res' );
+option_path = fullfile( res_path, 'legacy_demo_options.json' );
+options = Options( '', option_path, stl_path, output_path );
+[ ~, name, ~ ] = fileparts( stl_path );
+
+%% ANALYSIS
+try
+	tbl = legacy_run( options );
+	tbl.Properties.RowNames = { name };
+catch e
+	fprintf( 1, '%s\n', getReport( e ) );
+	fprintf( 1, '%s\n', stl_path );
+	return;
+end
+
+%% OUTPUT
+csv_name = sprintf( '%s.csv', name );
+output_file = fullfile( output_path, csv_name );
+writetable( tbl, output_file );
+
+end
