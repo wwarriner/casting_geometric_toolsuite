303e55b8193781340c3506e2a0707b28afb54624
UAB\wwarr <wwarr@uab.edu>
2018-11-16 12:27:51 -0600

    reduced to just regular discrete sampling

--- examples/optimization_demo/oo_hpc.m
+++ examples/optimization_demo/oo_hpc.m
@@ -13,33 +13,7 @@ s.legacy_run( e, m );
 f = Feeders();
 f.legacy_run( s, m );
 
-oo = MultiObjectiveOrientationOptimizer( c, f, op.element_count );
-objective_opt = ...
-    @(rotation_fn,angles)multiple_objective_opt( rotation_fn, angles );
-oo.run( objective_opt, op.population_count, op.generation_count );
-filename = [ 'oo_' c.name ];
-save( fullfile( output_mat_dir, filename ), 'oo' );
-
-orthogonal_angles = [ ...
-    0 0; ...
-    0 -pi/2; ...
-    0 pi/2; ...
-    pi/2 0; ...
-    pi/2 0; ...
-    pi 0 ...
-    ];
-count = size( orthogonal_angles, 1 );
-orth = nan( count, numel( multiple_objective_opt() ) );
-rot_fn = @(angles) oo.rotate_objects( c, f, op.element_count, angles );
-parfor i = 1 : count
-    
-    orth( i, : ) = objective_opt( rot_fn, orthogonal_angles( i, : ) );
-    
-end
-filename = [ 'orth_' c.name ];
-save( fullfile( output_mat_dir, filename ), 'orth' );
-
-sampled_angles = generate_sphere_angles( 100 );
+sampled_angles = generate_sphere_angles( 1000 );
 count = size( sampled_angles, 1 );
 samp = nan( count, numel( multiple_objective_opt() ) );
 parfor i = 1 : count
