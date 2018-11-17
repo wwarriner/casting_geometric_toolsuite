d55e7fe6926b6d833ca8074805cbf0b9d0ee9cad
UAB\wwarr <wwarr@uab.edu>
2018-11-17 15:18:09 -0600

    modified to include index arg, will also write csv files

--- examples/optimization_demo/oo_hpc.m
+++ examples/optimization_demo/oo_hpc.m
@@ -1,8 +1,6 @@
-function oo_hpc( input_path, option_path, angles, output_mat_dir )
+function oo_hpc( input_path, option_path, angles, index, output_mat_dir )
 
 op = Options( 'option_defaults.json', option_path, input_path, '' );
-
-r = rotator( angles );
 
 c = Component();
 c.legacy_run( input_path );
@@ -15,6 +13,7 @@ s.legacy_run( e, m );
 f = Feeders();
 f.legacy_run( s, m );
 
+r = rotator( angles );
 cr = c.rotate( r );
 mr = Mesh();
 mr.legacy_run( cr, op.element_count );
@@ -22,16 +21,13 @@ fr = f.rotate( r, mr );
 
 
 
-rot_fn = @(angles) deal( cr, mr, fr );
-%sampled_angles = generate_sphere_angles( 1000 );
-%count = size( sampled_angles, 1 );
-%samp = nan( count, numel( multiple_objective_opt() ) );
-%parfor i = 1 : count
-    
-samp = multiple_objective_opt( rot_fn, angles );
-    
-%end
-filename = [ 'samp_' c.name '_' sprintf( '%i', index ) '.mat' ];
-save( fullfile( output_mat_dir, filename ), 'samp' );
+rotation_function = @(angles) deal( cr, mr, fr );    
+objectives = multiple_objective_opt( rotation_function, angles );
+objectives = array2table( objectives );
+[ ~, titles ] = multiple_objective_opt();
+objectives.Properties.VariableNames = titles;
+
+filename = [ 'objectives_' c.name '_' sprintf( '%i', index ) '.csv' ];
+writetable( objectives, fullfile( output_mat_dir, filename ) );
 
 end
