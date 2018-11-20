55f22c1e7ea98951a2b556f25ee531913170a0fb
UAB\wwarr <wwarr@uab.edu>
2018-11-20 16:33:45 -0600

    updated to provide job id to tasks, also copying the objective variables file used with job id appended for use in postprocessing, for consistency

--- examples/optimization_demo/oo_project.sh
+++ examples/optimization_demo/oo_project.sh
@@ -9,7 +9,7 @@ CSV_NAME='sphere_angles.csv'
 CSV_PATH=$ROOT_DIR'/'$CSV_NAME
 OUTPUT_PATH=$ROOT_DIR'/results'
 OPTIONS_PATH=$ROOT_DIR'/oo_options.json'
-RUN_CMD='generate_csvs_on_hpc( '\'$STL_PATH\'', '\'$OPTIONS_PATH\'', [$ANGLES], $SLURM_ARRAY_TASK_ID, '\'$OUTPUT_PATH\'' );'
+RUN_CMD='generate_csvs_on_hpc( '\'$STL_PATH\'', '\'$OPTIONS_PATH\'', [$ANGLES], $SLURM_ARRAY_TASK_ID, $SLURM_JOB_ID, '\'$OUTPUT_PATH\'' );'
 FULL_CMD=$( create_matlab_command -a -c $CD_DIR -d $ROOT_DIR -f "$RUN_CMD" )
 
 NAME=oo_project
@@ -22,9 +22,16 @@ PARTITION=express
 MAILTYPE=FAIL
 MAILADDRESS='wwarr@uab.edu'
 
+
 module load rc/matlab/R2018a
-sbatch --array=0-$ARRAYMAX%$MAXTASKS --job-name $NAME --output=output/output_%A_%a.txt --ntasks=$TASKS --mem-per-cpu=$MEMORY --time=$TIME --partition=$PARTITION --mail-type=$MAILTYPE --mail-user=$MAILADDRESS <<LIMITING_STRING
+JOB_ID=$( sbatch --array=0-$ARRAYMAX%$MAXTASKS --job-name=$NAME --output=output/output_%A_%a.txt --ntasks=$TASKS --mem-per-cpu=$MEMORY --time=$TIME --partition=$PARTITION --mail-type=$MAILTYPE --mail-user=$MAILADDRESS <<LIMITING_STRING
 #!/bin/bash
 ANGLES=\$( sed \$(( \$SLURM_ARRAY_TASK_ID+1 ))'q;d' $CSV_PATH )
 matlab -nodisplay -nodesktop -sd $ROOT_DIR -r $FULL_CMD
-LIMITING_STRING
+LIMITING_STRING )
+
+OBJECTIVE_NAME='objective_variables.json'
+OBJECTIVE_PATH=$CD_DIR'/'$OBJECTIVE_NAME
+OBJECTIVE_CP_NAME='objective_variables_'$JOB_ID'.json'
+OBJECTIVE_CP_PATH=$CD_DIR'/'$OBJECTIVE_CP_NAME
+cp -fp $OBJECTIVE_PATH $OBJECTIVE_CP_PATH
