3e009ed69771e8febd0555be798bc506be819bdb
UAB\wwarr <wwarr@uab.edu>
2018-11-20 15:13:01 -0600

    refactored to take advantage of new base case class

--- examples/optimization_demo/generate_csvs_on_hpc.m
+++ examples/optimization_demo/generate_csvs_on_hpc.m
@@ -1,35 +1,8 @@
 function generate_csvs_on_hpc( input_path, option_path, angles, index, output_mat_dir )
 
-op = Options( 'option_defaults.json', option_path, input_path, '' );
-
-c = Component();
-c.legacy_run( input_path );
-m = Mesh();
-m.legacy_run( c, op.element_count );
-e = EdtProfile();
-e.legacy_run( m );
-s = Segmentation();
-s.legacy_run( e, m );
-f = Feeders();
-f.legacy_run( s, m );
-
-r = rotator( angles );
-cr = c.rotate( r );
-mr = Mesh();
-mr.legacy_run( cr, op.element_count );
-fr = f.rotate( r, mr );
-
-rotation_function = @(angles) deal( cr, mr, fr );    
-objectives = multiple_objective_opt( rotation_function, angles );
-results = [ angles( 1 ) angles( 2 ) objectives ];
-
-[ ~, titles ] = multiple_objective_opt();
-titles = [ 'phi' 'theta' titles ];
-
-results = array2table( results );
-results.Properties.VariableNames = titles;
-
-filename = [ 'results_' c.name '_' sprintf( '%i', index ) '.csv' ];
+opt = Optimizer( option_path, input_path );
+results = opt.determine_results_as_table( angles );
+filename = [ 'results_' opt.get_name() '_' sprintf( '%i', index ) '.csv' ];
 writetable( results, fullfile( output_mat_dir, filename ) );
 
 end
