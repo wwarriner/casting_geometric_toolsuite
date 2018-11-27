f6ab2f02863eac446fb9913d741a96cbcb2a6475
UAB\wwarr <wwarr@uab.edu>
2018-11-27 12:10:55 -0600

    made into full example script

--- examples/optimization_demo/plot_csv_data.m
+++ examples/optimization_demo/plot_csv_data.m
@@ -1,7 +1,12 @@
-% results_table = base_plate_data;
-% figure_resolution_px = 600;
-% usrd = UnitSphereResponseData( ...
-%     results_table, ...
-%     figure_resolution_px ...
-%     );
-usrp = UnitSphereResponsePlot( usrd, figure_resolution_px );
+load( 'base_plate_data.mat' );
+load( 'bearing_block_data.mat' );
+load( 'steering_column_mount_data.mat' );
+
+results_table = base_plate_data;
+figure_resolution_px = 600;
+usra = UnitSphereResponseAxes();
+usrd = UnitSphereResponseData( ...
+    results_table, ...
+    figure_resolution_px ...
+    );
+usrp = UnitSphereResponsePlot( usrd, usra, figure_resolution_px );
