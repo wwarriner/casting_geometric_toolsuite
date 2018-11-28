4a97c0d0cea8d001cfc55d36ced4d2ff51e06c93
UAB\wwarr <wwarr@uab.edu>
2018-11-28 17:25:13 -0600

    copy options, objectives, angles, stl to results folder, with job_id appended to name

--- examples/optimization_demo/hpc/oo_project.sh
+++ examples/optimization_demo/hpc/oo_project.sh
@@ -2,16 +2,20 @@
 ROOT_DIR=$(dirname "$(realpath "$0")")
 PATH=$PATH:"$ROOT_DIR"
 
-CD_DIR=$ROOT_DIR'/repos/casting_geometric_toolsuite'
+REPO_DIR=$ROOT_DIR'/repos/casting_geometric_toolsuite'
+OPT_DEMO_DIR=$REPO_DIR'/examples/optimization_demo'
 STL_NAME=$1
 STL_PATH=$ROOT_DIR'/oo_stl/'$STL_NAME
+OPTIONS_NAME='oo_options.json'
+OPTIONS_PATH=$OPT_DEMO_DIR'/'$OPTIONS_NAME
+OBJECTIVE_NAME='objective_variables.json'
+OBJECTIVE_PATH=$OPT_DEMO_DIR'/'$OBJECTIVE_NAME
+OUTPUT_PATH=$ROOT_DIR'/results'
+RUN_CMD='generate_csvs_on_hpc( '\'$STL_PATH\'', '\'$OPTIONS_PATH\'', '\'$OBJECTIVE_PATH\'', [$ANGLES], $SLURM_ARRAY_TASK_ID, $SLURM_JOB_ID, '\'$OUTPUT_PATH\'' );'
+FULL_CMD=$( create_matlab_command -a -c $REPO_DIR -d $ROOT_DIR -f "$RUN_CMD" )
+
 CSV_NAME='sphere_angles.csv'
 CSV_PATH=$ROOT_DIR'/'$CSV_NAME
-OUTPUT_PATH=$ROOT_DIR'/results'
-OPTIONS_PATH=$ROOT_DIR'/oo_options.json'
-RUN_CMD='generate_csvs_on_hpc( '\'$STL_PATH\'', '\'$OPTIONS_PATH\'', [$ANGLES], $SLURM_ARRAY_TASK_ID, $SLURM_JOB_ID, '\'$OUTPUT_PATH\'' );'
-FULL_CMD=$( create_matlab_command -a -c $CD_DIR -d $ROOT_DIR -f "$RUN_CMD" )
-
 NAME=oo_project
 ARRAYMAX=$(( $( wc -l < $CSV_PATH ) -1 ))
 MAXTASKS=64
@@ -22,16 +26,30 @@ PARTITION=express
 MAILTYPE=FAIL
 MAILADDRESS='wwarr@uab.edu'
 
-
 module load rc/matlab/R2018a
-JOB_ID=$( sbatch --array=0-$ARRAYMAX%$MAXTASKS --job-name=$NAME --output=output/output_%A_%a.txt --ntasks=$TASKS --mem-per-cpu=$MEMORY --time=$TIME --partition=$PARTITION --mail-type=$MAILTYPE --mail-user=$MAILADDRESS <<LIMITING_STRING
+JOB_ID=$( sbatch --array=0-1%$MAXTASKS --job-name=$NAME --output=output/output_%A_%a.txt --ntasks=$TASKS --mem-per-cpu=$MEMORY --time=$TIME --partition=$PARTITION --mail-type=$MAILTYPE --mail-user=$MAILADDRESS <<LIMITING_STRING
 #!/bin/bash
 ANGLES=\$( sed \$(( \$SLURM_ARRAY_TASK_ID+1 ))'q;d' $CSV_PATH )
 matlab -nodisplay -nodesktop -sd $ROOT_DIR -r $FULL_CMD
-LIMITING_STRING )
+LIMITING_STRING
+)
+JOB_ID=${JOB_ID##* }
+printf '%s\n' "$JOB_ID"
 
-OBJECTIVE_NAME='objective_variables.json'
-OBJECTIVE_PATH=$CD_DIR'/'$OBJECTIVE_NAME
-OBJECTIVE_CP_NAME='objective_variables_'$JOB_ID'.json'
-OBJECTIVE_CP_PATH=$CD_DIR'/'$OBJECTIVE_CP_NAME
-cp -fp $OBJECTIVE_PATH $OBJECTIVE_CP_PATH
+copy_file()
+{
+	DIR=`dirname "$1"`
+	BASENAME=`basename "$1"`
+	EXT=".${BASENAME##*.}"
+	NAME="${BASENAME%.*}"
+	
+	OUT_DIR="$2"
+	JOB_ID="$3"
+	CP_PATH="$OUT_DIR"'/'"$NAME"'_'"$JOB_ID""$EXT"
+	cp -fp "$1" "$CP_PATH"
+}
+
+copy_file "$OBJECTIVE_PATH" "$OUTPUT_PATH" "$JOB_ID"
+copy_file "$STL_PATH" "$OUTPUT_PATH" "$JOB_ID"
+copy_file "$CSV_PATH" "$OUTPUT_PATH" "$JOB_ID"
+copy_file "$OPTIONS_PATH" "$OUTPUT_PATH" "$JOB_ID"
