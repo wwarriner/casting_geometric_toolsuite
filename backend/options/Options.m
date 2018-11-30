e990d92b0642dcf9b3faded50d88ba9c2b52cd1a
UAB\wwarr <wwarr@uab.edu>
2018-11-30 17:53:33 -0600

    refactored to remove unneeded function

--- backend/options/Options.m
+++ backend/options/Options.m
@@ -5,13 +5,13 @@ classdef (Sealed) Options < dynamicprops
         function obj = Options( option_defaults_path, varargin )
             
             if ~isempty( option_defaults_path )
-                Options.update_values( option_defaults_path, @obj.assign_default );
+                Options.update_values( option_defaults_path, @obj.assign_properties );
             end
             
             if nargin == 2
-                Options.update_values( varargin{ 1 }, @obj.assign_user );
+                Options.update_values( varargin{ 1 }, @obj.assign_properties );
             elseif nargin == 4
-                Options.update_values( varargin{ 1 }, @obj.assign_user );
+                Options.update_values( varargin{ 1 }, @obj.assign_properties );
                 obj.assign_user( 'input_stl_path', varargin{ 2 } );
                 obj.assign_user( 'output_path', varargin{ 3 } );
             else
@@ -25,20 +25,9 @@ classdef (Sealed) Options < dynamicprops
     
     methods ( Access = private )
         
-        function assign_default( obj, key, value )
+        function assign_properties( obj, key, value )
             
             obj.add_property( key );
-            obj.(key) = value;
-            
-        end
-        
-        
-        function assign_user( obj, key, value )
-            
-            if ~isprop( obj, key )
-                %warning( '%s is not a default option\n', key );
-                obj.add_property( key );
-            end
             obj.(key) = value;
             
         end
