e28df7574ddc7d93060aecde7c3cc24dd90041c0
UAB\wwarr <wwarr@uab.edu>
2018-12-07 18:57:38 -0600

    added removal of sufficiently small undercuts (vol/mesh_vol ratio <10^-4)

--- backend/image_utils/remove_small_connected_regions.m
+++ backend/image_utils/remove_small_connected_regions.m
@@ -13,10 +13,13 @@ segments = double( labelmatrix( connected_regions ) );
 segment_count = connected_regions.NumObjects;
 
 %% COMPUTE RATIOS
-segment_element_counts = arrayfun( @(y)sum(segments(:)==y), 1 : segment_count );
+segment_element_counts = arrayfun( @(y)sum( segments( : ) == y ), 1 : segment_count );
+segment_element_ratios = segment_element_counts ./ prod( size( binary_image ) );
 
 %% THRESHOLD REGIONS BY RATIO
-indices_to_remove = isoutlier( 1 ./ segment_element_counts, 'quartiles' );
+indices_to_remove = ...
+    isoutlier( 1 ./ segment_element_counts, 'quartiles' ) ...
+    | segment_element_ratios < 1e-4;
 adjusted_image = segments;
 for i = 1 : segment_count
     if indices_to_remove( i )
