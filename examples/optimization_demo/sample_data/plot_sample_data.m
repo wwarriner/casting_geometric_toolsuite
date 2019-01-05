065b925a5cb70fa3a30535230fcce624f3529788
UAB\wwarr <wwarr@uab.edu>
2019-01-04 18:49:46 -0600

    updated to take advantage of new functionality to produce publication plots

--- examples/optimization_demo/sample_data/plot_sample_data.m
+++ examples/optimization_demo/sample_data/plot_sample_data.m
@@ -12,9 +12,13 @@ data.Properties.UserData.StlPath = which( data.Properties.UserData.StlPath );
 data.Properties.UserData.OptionsPath = which( data.Properties.UserData.OptionsPath );
 
 figure_resolution_px = 600;
-usra = UnitSphereResponseAxes();
+color_map = interp1( [ 0; 1 ], repmat( [ 0.3; 0.9 ], [ 1 3 ] ), linspace( 0, 1, 256 ) );
+grid_color = [ 0 0 0 ];
+usra = UnitSphereResponseAxes( color_map, grid_color );
 usrd = UnitSphereResponseData( data, figure_resolution_px );
 usrp = UnitSphereResponsePlot( usrd, usra, figure_resolution_px );
+bg_color = [ 1 1 1 ];
+usrp.set_background_color( bg_color );
 
 end
 
