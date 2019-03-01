e91d8fb54d6267f5db35f7e05e56011105bbdb6a
UAB\wwarr <wwarr@uab.edu>
2019-02-28 19:47:54 -0600

    updated to keep pace with development

--- testing/constant_property_test_case.m
+++ testing/constant_property_test_case.m
@@ -2,20 +2,22 @@
 ambient_id = 0;
 mold_id = 1;
 melt_id = 2;
-element_size_in_mm = 100; % mm
-simulation_time_step_in_s = 100; % s
+element_size_in_mm = 10; % mm
+simulation_time_step_in_s = 5; % s
 
 %% TEST MESH GENERATION
 side_length = 50;
 shape = [ ...
     side_length ...
-    2 * side_length ...
-    side_length / 2 ...
+    side_length ...
+    side_length ...
     ];
 [ fdm_mesh, center ] = generate_test_mesh( mold_id, melt_id, shape );
 
 %% TEST PROPERTY GENERATION
-pp = generate_constant_test_properties( ambient_id, mold_id, melt_id );
+melt_fp = which( 'AlSi9.txt' );
+pp = generate_variable_test_properties( ambient_id, mold_id, melt_id, melt_fp );
+% pp = generate_constant_test_properties( ambient_id, mold_id, melt_id );
 pp.set_space_step( element_size_in_mm / 1000 ) % m
 pp.set_max_length( shape ); % count
 pp.prepare_for_solver();
