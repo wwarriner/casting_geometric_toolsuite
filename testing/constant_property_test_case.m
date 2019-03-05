f527ae1d133061674bc32254fe938b01363488d1
UAB\wwarr <wwarr@uab.edu>
2019-03-05 15:02:02 -0600

    enhancements

--- testing/constant_property_test_case.m
+++ testing/constant_property_test_case.m
@@ -24,10 +24,10 @@ pp.prepare_for_solver();
 %% MATRIX GENERATOR
 lss = LinearSystemSolver( fdm_mesh, pp );
 lss.set_implicitness_factor( 1 );
-lss.set_solver_tolerance( 1e-6 );
+lss.set_solver_tolerance( 1e-3 );
 lss.set_solver_max_iteration_count( 100 );
 lss.set_adaptive_time_step_relaxation_parameter( 0.5 );
-lss.set_latent_heat_target_fraction( 0.5 );
+lss.set_latent_heat_target_fraction( 0.25 );
 lss.set_quality_ratio_tolerance( 0.1 );
 
 %% SOLVER
@@ -35,3 +35,4 @@ solver = Solver( fdm_mesh, pp, lss );
 solver.turn_printing_on( @fprintf );
 solver.turn_live_plotting_on();
 solver.solve( melt_id );
+solver.display_computation_time_summary();
