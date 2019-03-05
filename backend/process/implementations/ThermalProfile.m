98040a5b9074991f12565035772f9966573549b5
UAB\wwarr <wwarr@uab.edu>
2019-03-04 20:10:17 -0600

    updated to match recent interface version of fdm solver

--- backend/process/implementations/ThermalProfile.m
+++ backend/process/implementations/ThermalProfile.m
@@ -4,7 +4,6 @@ classdef ThermalProfile < Process
         %% inputs
         mesh
         physical_properties
-        simulation_time_step_in_s
         
         %% outputs
         solidification_times
@@ -41,7 +40,6 @@ classdef ThermalProfile < Process
             
             assert( ~isempty( obj.mesh ) );
             assert( ~isempty( obj.physical_properties ) );
-            assert( ~isempty( obj.simulation_time_step_in_s ) );
             
             obj.printf( 'Computing thermal profile...\n' );
             
@@ -55,27 +53,20 @@ classdef ThermalProfile < Process
             obj.physical_properties.set_max_length( obj.mesh.shape  ); % count
             obj.physical_properties.prepare_for_solver();
             pp = obj.physical_properties;
-            mg = MatrixGenerator( ...
-                fdm_mesh, ...
-                ambient_id, ...
-                @pp.lookup_rho_cp_nd, ...
-                @pp.lookup_k_nd_half_space_step_inv, ...
-                @pp.lookup_h_nd ...
-                );
-            solver = Solver( fdm_mesh, obj.physical_properties, mg );
-            solver.turn_printing_on();
+            lss = LinearSystemSolver( fdm_mesh, pp );
+            solver = Solver( fdm_mesh, obj.physical_properties, lss );
+            solver.turn_printing_on( @obj.printf );
             solver.turn_live_plotting_on();
-            solver.solve( obj.simulation_time_step_in_s, melt_id );
+            solver.solve( melt_id );
             obj.solidification_times = solver.solidification_times;
             
         end
         
         
-        function legacy_run( obj, mesh, physical_properties, simulation_time_step_in_s )
+        function legacy_run( obj, mesh, physical_properties )
             
             obj.mesh = mesh;
             obj.physical_properties = physical_properties;
-            obj.simulation_time_step_in_s = simulation_time_step_in_s;
             obj.run();
             
         end
--- dev_testing/thermal_profile_test.m
+++ dev_testing/thermal_profile_test.m
@@ -2,10 +2,9 @@ c = Component();
 c.legacy_run( which( 'bearing_block.stl' ) );
 m = Mesh();
 m.legacy_run( c, 1e6 );
-pp = generate_constant_test_properties( 0, 1, 2 );
-time_step_in_s = 0.01;
+pp = generate_variable_test_properties( 0, 1, 2, which( 'AlSi9.txt' ) );
 tp = ThermalProfile();
-tp.legacy_run( m, pp, time_step_in_s );
+tp.legacy_run( m, pp );
 
 % TODO
 %  - unpad in TP so we can WS etc down the line
