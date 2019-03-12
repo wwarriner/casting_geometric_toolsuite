6a130a580ad9214a9e2cd4d4592d40fcea40912f
UAB\wwarr <wwarr@uab.edu>
2019-03-12 18:03:02 -0500

    added culling to FsProperty to trim to only liquidus-solidus range, since we use nearest extrapolation, we don't need the other data

--- material_properties/FsProperty.m
+++ material_properties/FsProperty.m
@@ -7,6 +7,13 @@ classdef (Sealed) FsProperty < MaterialProperty
             
             obj = obj@MaterialProperty( temperatures, fractions_solid );
             
+            if ~isnan( obj.temperatures )
+                first_liquid = find( obj.values == 0, 1, 'first' );
+                last_solid = find( obj.values == 1, 1, 'last' );
+                obj.temperatures = obj.temperatures( last_solid : first_liquid );
+                obj.values = obj.values( last_solid : first_liquid );
+            end
+            
             assert( obj.values( end ) == 0.0 ); % fully liquid at >= max temp
             assert( obj.values( 1 ) == 1.0 ); % fully solid at <= min temp
             
