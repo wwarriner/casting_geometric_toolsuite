bfe82ea36e1c7ddc7d78620ee52d1a8888805d47
UAB\wwarr <wwarr@uab.edu>
2019-04-08 15:59:54 -0500

    rebuilt backend

--- backend/res/process_implementation_includer.m
+++ backend/res/process_implementation_includer.m
@@ -3,12 +3,15 @@ function process_implementation_includer()
 BottleCores();
 CavityThinWall();
 Component();
+CoreDirectional();
+CoreIsotropic();
 EdtProfile();
 Feeders();
 Mesh();
 MoldThinWall();
 PartingPerimeter();
 Segmentation();
+ThermalProfile();
 Undercuts();
 Waterfall();
 
