e6ccaff19c79cdd5f9cfda6768bbb96cc2f395ef
UAB\wwarr <wwarr@uab.edu>
2019-05-14 18:51:48 -0500

    in process of updating options, do not use

--- backend/options/Options.m
+++ backend/options/Options.m
@@ -1,21 +1,23 @@
-classdef (Sealed) Options < dynamicprops
+classdef (Sealed) Options < Property
     
     methods ( Access = public )
         
         function obj = Options( option_defaults_path, varargin )
             
             if ~isempty( option_defaults_path )
-                Options.update_values( option_defaults_path, @obj.assign_properties );
+                obj.create( option_defaults_path );
             end
             
-            if nargin == 2
-                Options.update_values( varargin{ 1 }, @obj.assign_properties );
-            elseif nargin == 4
-                Options.update_values( varargin{ 1 }, @obj.assign_properties );
-                obj.assign_properties( 'input_stl_path', varargin{ 2 } );
-                obj.assign_properties( 'output_path', varargin{ 3 } );
-            else
-                error( 'Incorrect arguments' );
+            if nargin >= 2
+                obj.update( varargin{ 1 } );
+            end
+            
+            if nargin >= 3
+                obj.stl_path.set( varargin{ 2 } );
+            end
+            
+            if nargin >= 4
+                obj.output_path.set( varargin{ 3 } );
             end
             
         end
@@ -25,38 +27,65 @@ classdef (Sealed) Options < dynamicprops
     
     methods ( Access = private )
         
-        function assign_properties( obj, key, value )
+        function create( obj, path )
             
-            obj.add_property( key );
-            obj.(key) = value;
+            values = jsondecode( fileread( path ) );
+            obj.add_props( values );
             
         end
         
         
-        function add_property( obj, key )
+        function add_props( obj, value, keys )
             
-            if ~isprop( obj, key )
-                P = addprop( obj, key );
-                P.Access = 'public';
+            if nargin < 3
+                keys = {};
+            end
+            
+            if ~isa( value, 'struct' )
+                obj.add( strjoin( keys, '.' ), value );
+            else
+                fields = fieldnames( value );
+                for i = 1 : numel( fields )
+                    
+                    field = fields{ i };
+                    obj.add_props( ...
+                        value.(field), ...
+                        [ keys { field } ] ...
+                        );
+                    
+                end
             end
             
         end
         
-    end
-    
-    
-    methods ( Access = private, Static )
         
-        function update_values( path, update_func )
+        function update( obj, path )
             
             values = jsondecode( fileread( path ) );
-            fn = fieldnames( values );
-            for i = 1 : numel( fn )
-                
-                key = fn{ i };
-                value = values.(key);
-                update_func( key, value );
-                
+            obj.set_props( values );
+            
+        end
+        
+        
+        function set_props( obj, value, keys )
+            
+            if nargin < 3
+                keys = {};
+            end
+            
+            if ~isa( value, 'struct' )
+                obj.set( strjoin( keys, '.' ), value );
+            else
+                fields = fieldnames( value );
+                for i = 1 : numel( fields )
+                    
+                    field = fields{ i };
+                    obj.set_props( ...
+                        value.(field), ...
+                        [ keys { field } ] ...
+                        );
+                    
+                end
             end
             
         end
---
+++ backend/options/Property.m
@@ -0,0 +1,120 @@
+classdef Property < dynamicprops
+    
+    methods ( Access = public )
+        
+        function add( obj, key, value )
+            
+            if isempty( key ); return; end
+            
+            [ first, rest ] = obj.split( key );
+            if isprop( obj, first ) && ~isempty( rest )
+                obj.(first).add( rest, value );
+            else
+                p = obj.addprop( first );
+                if isempty( rest )
+                    sub = value;
+                else
+                    sub = Property();
+                    sub.add( rest, value );
+                end
+                obj.(first) = sub;
+                p.GetAccess = 'public';
+                p.SetAccess = 'private';
+            end
+            
+        end
+        
+        
+        function set( obj, key, value )
+            
+            obj.set_impl( key, key, value );
+            
+        end
+        
+        
+        function value = get( obj, key, fallback )
+            
+            if nargin < 3
+                fallback = [];
+            end
+            
+            % TODO improve fallback
+            value = obj.get_impl( key, key );
+            if ~isempty( fallback ) && isempty( value )
+                value = fallback;
+            end
+            
+        end
+        
+    end
+    
+    
+    methods ( Access = private )
+        
+        function set_impl( obj, full_key, key, value )
+            
+            assert( ~isempty( key ) );
+            
+            [ first, rest ] = obj.split( key );
+            if ~isprop( obj, first )
+                warning( 'Cannot set unknown key: %s\n', full_key );
+                return;
+            end
+            
+            if isempty( rest )
+                if isa( obj.(first), 'Property' )
+                    warning( 'Cannot overwrite subproperty: %s\n', full_key );
+                    return;
+                else
+                    obj.(first) = value;
+                end
+            else
+                if ~isa( obj.(first), 'Property' )
+                    warning( 'Cannot overwrite value: %s\n', full_key );
+                    return;
+                else
+                    obj.(first).set_impl( full_key, rest, value );
+                end
+            end
+            
+        end
+        
+        
+        function value = get_impl( obj, full_key, key )
+            
+            assert( ~isempty( key ) );
+            
+            [ first, rest ] = obj.split( key );
+            if ~isprop( obj, first )
+                warning( 'Cannot get unknown key: %s\n', full_key );
+                return;
+            end
+            
+            if isempty( rest )
+                value = obj.(first);
+            else
+                value = obj.(first).get_impl( full_key, rest );
+            end
+            
+        end
+        
+    end
+    
+    
+    methods ( Access = private, Static )
+        
+        function [ first, rest ] = split( key )
+            
+            first = extractBefore( key, '.' );
+            if isempty( first ); first = key; end
+            if ~isvarname( first )
+                error( 'Invalid key: %s\n', first );
+            end
+            rest = extractAfter( key, '.' );
+            
+        end
+        
+    end
+    
+end
+
