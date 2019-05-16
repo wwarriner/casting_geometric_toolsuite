b32dc3edd118691dcae24b0a8c5f094971fc7082
UAB\wwarr <wwarr@uab.edu>
2019-05-15 21:00:12 -0500

    removed unnecessary definitions

--- materials/AmbientMaterial.m
+++ materials/AmbientMaterial.m
@@ -1,14 +1,6 @@
 classdef (Sealed) AmbientMaterial < Material
     
-    methods ( Access = public )
-        
-        function obj = AmbientMaterial( ambient_id )
-            
-            obj = obj@Material( ambient_id );
-            
-        end
-        
-    end
+    % no definition required
     
 end
 
--- materials/MoldMaterial.m
+++ materials/MoldMaterial.m
@@ -1,14 +1,6 @@
 classdef (Sealed) MoldMaterial < Material
     
-    methods ( Access = public )
-        
-        function obj = MoldMaterial( mold_id )
-            
-            obj = obj@Material( mold_id );
-            
-        end
-        
-    end
+    % no definition required
     
 end
 
