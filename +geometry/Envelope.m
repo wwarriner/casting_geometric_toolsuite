a239663480f01509dea3afe17e5f151a2cfc2353
William Warriner <wwarriner@gmail.com>
2019-07-19 17:44:52 -0500

    cleanup

--- +geometry/Envelope.m
+++ +geometry/Envelope.m
@@ -1,27 +1,23 @@
-classdef (Sealed) Envelope < handle & matlab.mixin.Copyable
+classdef (Sealed) Envelope < handle
     
-    properties ( GetAccess = public, SetAccess = private )
-        
+    properties ( SetAccess = private )
         dimension_count(1,1) uint64 {mustBePositive} = 1
         min_point(1,:) double {mustBeReal,mustBeFinite} = []
         max_point(1,:) double {mustBeReal,mustBeFinite} = []
         lengths(1,:) double {mustBeReal,mustBeFinite,mustBeNonnegative} = []
         volume(1,1) double {mustBeReal,mustBeFinite,mustBeNonnegative} = 0.0
-        
     end
     
     methods ( Access = public )
         
         % any one object like an fv struct
-        % rows of fv.vertices must be points, columns dimensions/axes
+        % rows of fv.vertices must be points, columns dimensions
         % -OR-
-        % min_point, max_point as two vectors of length 3
+        % min_point, max_point as two vectors
         function obj = Envelope( varargin )
-            
             if nargin == 0
                 return;
             end
-            
             if 2 < nargin
                 assert( false )
             end
@@ -34,18 +30,14 @@ classdef (Sealed) Envelope < handle & matlab.mixin.Copyable
             else
                 assert( false );
             end
-            
         end
         
-        
         function u = union( obj, envelope )
-            
             assert( obj.dimension_count == envelope.dimension_count );
             
             new_min = min( obj.min_point, envelope.min_point );
             new_max = max( obj.max_point, envelope.max_point );
             u = geometry.Envelope( new_min, new_max );
-            
         end
         
     end
@@ -54,7 +46,6 @@ classdef (Sealed) Envelope < handle & matlab.mixin.Copyable
     methods ( Access = private )
         
         function construct_from_fv( obj, fv )
-            
             assert( isstruct( fv ) || isobject( fv ) );
             if isstruct( fv )
                 assert( isfield( fv, 'vertices' ) );
@@ -65,40 +56,29 @@ classdef (Sealed) Envelope < handle & matlab.mixin.Copyable
             obj.dimension_count = size( fv.vertices, 2 );
             obj.min_point = min( fv.vertices, [], 1 );
             obj.max_point = max( fv.vertices, [], 1 );
-            
             obj.recompute();
-            
         end
         
         
         function construct_from_points( obj, varargin )
-            
             assert( nargin == 3 )
             
             min = varargin{ 1 };
             max = varargin{ 2 };
-            
-            assert( isnumeric( min ) && isnumeric( max ) );
-            assert( isvector( min ) && isvector( max ) );
             assert( numel( min ) == numel( max ) );
-            assert( all( isfinite( min ) ) && all( isfinite( max ) ) );
             
             obj.dimension_count = numel( min );
-            obj.min_point = min( : ).';
-            obj.max_point = max( : ).';
-            
+            obj.min_point = min;
+            obj.max_point = max;
             obj.recompute();
-            
         end
         
         
         function recompute( obj )
-            
             obj.min_point = min( obj.min_point, obj.max_point );
             obj.max_point = max( obj.min_point, obj.max_point );
             obj.lengths = obj.max_point - obj.min_point;
             obj.volume = prod( obj.lengths );
-            
         end
         
     end
