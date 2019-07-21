a3ce7b09a5d1a242de55bb26c20a2aa9952f76dd
William Warriner <wwarriner@gmail.com>
2019-07-21 11:32:41 -0500

    Created solver interface, small refactors

--- +solver/LinearSystemSolver.m
+++ +solver/LinearSystemSolver.m
@@ -1,66 +1,19 @@
-classdef (Sealed) LinearSystemSolver < handle
+classdef (Sealed) LinearSystemSolver < solver.SolverInterface
     
-    % TODO create interface
-    
-    properties ( GetAccess = public, SetAccess = private )
-        iteration_count(1,1) uint64 {mustBeNonnegative} = 0
-        time(1,1) double {mustBeReal,mustBeFinite,mustBeNonnegative} = 0.0
-    end
-    
-    properties ( Access = public )
-        tolerance(1,1) double {mustBeReal,mustBeFinite,mustBePositive} = 1e-4
-        maximum_iteration_count(1,1) uint64 {mustBePositive} = 100
-    end
-    
-    methods ( Access = public )
-        
-        function x = solve( obj, A, b, x0 )
-            if nargin < 4
-                x0 = [];
-            end
-            
-            assert( ismatrix( A ) )
-            
-            assert( isvector( b ) )
-            assert( length( b ) == size( A, 1 ) );
-            
-            if ~isempty( x0 )
-                assert( isvector( x0 ) )
-                assert( length( x0 ) == size( A, 1 ) );
-            end
-            
-            obj.reset();
-            
-            tic;
-            x = obj.solve_impl( A, b, x0 );
-            obj.time = toc;
-        end
-        
-    end
-    
-    
-    methods ( Access = private )
-        
-        function reset( obj )
-            obj.iteration_count = 0;
-            obj.time = 0.0;
-        end
-        
-        function x = solve_impl( obj, A, b, x0 )
+    methods ( Access = protected )
+        function [ x, iteration_count ] = solve_impl( obj, A, b, x0 )
             opts.michol = 'on';
             preconditioner = ichol( A, opts );
-            [ x, ~, ~, obj.iteration_count, ~ ] = pcg( ...
+            [ x, ~, ~, iteration_count, ~ ] = pcg( ...
                 A, ...
                 b, ...
                 obj.tolerance, ...
                 obj.maximum_iteration_count, ...
                 preconditioner, ...
                 preconditioner.', ...
-                x0 ... % empty case handle internally
+                x0 ... % empty case handled internally
                 );
-            
         end
-        
     end
     
 end
