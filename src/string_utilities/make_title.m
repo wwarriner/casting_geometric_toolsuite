51a3297452e9ebb748f8a5fdc3aeeff661f6d4b2
PORTABLE\William Warriner <wwarriner@gmail.com>
2019-09-11 13:25:51 -0500

    Added documentation and improved to accept cellstr

--- src/string_utilities/make_title.m
+++ src/string_utilities/make_title.m
@@ -1,13 +1,46 @@
+%{
+
+@make_title
+
+Description:
+This function converts strings to title-like strings. Every word in the 
+string has the first character capitalized if it is a letter, and every
+other letter character set to lower case. All other characters are removed.
+
+Inputs:
+ - @s, a string-like array. May be a string array, a character array, or a
+ cell array of character vectors.
+
+Outputs:
+ - @s, a string-like array of the same type as the input, converted as
+ described above.
+
+Examples:
+ - "a tAle of TWO cities" -> "A Tale Of Two Cities"
+ - "123 ABC" -> "123 Abc"
+
+%}
+
 function s = make_title( s )
 
-was_char = false;
-if ischar( s )
-    s = string( s );
-    was_char = true;
-end
+STRING = "string";
+CHAR = "char";
+CELLSTR = "cellstr";
 
+if isstring( s )
+    input_type = STRING;
+elseif ischar( s )
+    input_type = CHAR;
+elseif iscellstr( s )
+    input_type = CELLSTR;
+else
+    assert( false );
+end
+s = string( s );
+    
 bad_pattern = "([^0-9a-zA-Z]*)";
-s = lower( regexprep( s, bad_pattern, " " ) );
+s = regexprep( s, bad_pattern, " " );
+s = lower( s );
 
 initial_pattern = "([a-z])[0-9a-z]*";
 start = regexp( s, initial_pattern );
@@ -20,8 +53,15 @@ for i = 1 : numel( start )
     s{ i } = t;
 end
 
-if was_char
-    s = char( s );
+switch input_type
+    case CHAR
+        s = char( s );
+    case CELLSTR
+        s = cellstr( s );
+    case STRING
+        s = string( s );
+    otherwise
+        assert( false );
 end
 
 end
