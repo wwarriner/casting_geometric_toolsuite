f6520ef1f19c2ab61521d8dbf02936eb6ac7cd24
UAB\wwarr <wwarr@uab.edu>
2019-09-11 18:37:11 -0500

    triangle area computation now accepts fv or arrays of faces, vertices

--- src/geometry_utilities/compute_triangle_areas.m
+++ src/geometry_utilities/compute_triangle_areas.m
@@ -1,12 +1,31 @@
-function areas = compute_triangle_areas( fv )
+function areas = compute_triangle_areas( varargin )
 
-assert( isstruct( fv ) );
-assert( isfield( fv, 'faces' ) );
-assert( isfield( fv, 'vertices' ) );
+if nargin == 1
+    fv = varargin{ 1 };
+    assert( isstruct( fv ) );
+    assert( isfield( fv, 'faces' ) );
+    assert( isfield( fv, 'vertices' ) );
+    f = fv.faces;
+    v = fv.vertices;
+elseif nargin == 2
+    f = varargin{ 1 };
+    v = varargin{ 2 };
+else
+    assert( false );
+end
 
-u = fv.vertices( fv.faces( :, 3 ), : ) - fv.vertices( fv.faces( :, 1 ), : );
-v = fv.vertices( fv.faces( :, 3 ), : ) - fv.vertices( fv.faces( :, 2 ), : );
-areas = 0.5 .* vecnorm( cross( u, v ), 2, 2 );
+assert( isa( f, "double" ) );
+assert( isreal( f ) );
+assert( all( isfinite( f ), "all" ) );
+assert( all( 0.0 < f, "all" ) );
+
+assert( isa( v, "double" ) );
+assert( isreal( v ) );
+assert( all( isfinite( v ), "all" ) );
+
+a = v( f( :, 3 ), : ) - v( f( :, 1 ), : );
+b = v( f( :, 3 ), : ) - v( f( :, 2 ), : );
+areas = 0.5 .* vecnorm( cross( a, b ), 2, 2 );
 
 end
 
