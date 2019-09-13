83fc941de1c2f32c7bca3a245f106fb0ba1e008f
William Warriner <wwarriner@gmail.com>
2019-09-13 14:41:44 -0500

    refactored BasicMold

--- backend/process/in_progress/BasicMold.m
+++ backend/process/in_progress/BasicMold.m
@@ -1,40 +1,43 @@
 classdef (Sealed) BasicMold < handle & PrettyAxes3D
     
-    methods ( Access = public )
-        
+    methods
         function obj = BasicMold( varargin )
-            
             obj = obj@PrettyAxes3D( varargin{ : } );
-            
         end
         
-        function handles = draw( obj, axes_handle )
-            
-            state_restorer = obj.hold_if_not_held( axes_handle ); %#ok<NASGU>
-            
-            extrema = obj.get_extrema( axes_handle );
-            [ min_point, max_point ] = obj.extend_extrema( extrema, obj.MOLD_SCALING_FACTOR );
-            handles( 1 ) = obj.plot_box( axes_handle, min_point, max_point, min_point, obj.DRAG_COLOR );
-            handles( 2 ) = obj.plot_box( axes_handle, min_point, max_point, max_point, obj.COPE_COLOR );
-            
+        function handles = draw( obj, axh )
+            state_restorer = obj.hold_if_not_held( axh ); %#ok<NASGU>
+            extrema = obj.get_extrema( axh );
+            [ min_point, max_point ] = obj.extend_extrema( ...
+                extrema, ...
+                obj.MOLD_SCALING_FACTOR ...
+                );
+            handles( 1 ) = obj.plot_box( ...
+                axh, ...
+                min_point, ...
+                max_point, ...
+                min_point, ...
+                obj.DRAG_COLOR ...
+                );
+            handles( 2 ) = obj.plot_box( ...
+                axh, ...
+                min_point, ...
+                max_point, ...
+                max_point, ...
+                obj.COPE_COLOR ...
+                );
         end
-        
     end
-    
-    
-    properties ( Access = private, Constant )
-        
-        MOLD_SCALING_FACTOR = 1.05;
-        DRAG_COLOR = [ 0.0 1.0 0.25 ];
-        COPE_COLOR = [ 0.25 1.0 0.0 ];
-        
-    end
-    
     
     methods ( Access = private )
-        
-        function ph = plot_box( obj, axes_handle, min_point, max_point, extreme, color )
-            
+        function ph = plot_box( ...
+                obj, ...
+                axes_handle, ...
+                min_point, ...
+                max_point, ...
+                extreme, ...
+                color ...
+                )
             origin = obj.get_origin();
             box.vertices = [ ...
                 min_point( 1 ) min_point( 2 ) extreme( 3 ); ...
@@ -46,23 +49,20 @@ classdef (Sealed) BasicMold < handle & PrettyAxes3D
                 max_point( 1 ) max_point( 2 ) origin( 3 ); ...
                 min_point( 1 ) max_point( 2 ) origin( 3 ); ...
                 ];
-            box.faces = obj.get_faces();
+            box.faces = obj.FACES;
             ph = patch( axes_handle, box );
             ph.FaceColor = color;
             ph.FaceAlpha = 0.1;
             ph.EdgeColor = 'none';
             ph.SpecularStrength = 0.0;
-            
         end
-        
     end
     
-    
-    methods ( Access = private, Static )
-        
-        function faces = get_faces()
-            
-            faces = [ ...
+    properties ( Access = private, Constant )
+        MOLD_SCALING_FACTOR = 1.05;
+        DRAG_COLOR = [ 0.0 1.0 0.25 ];
+        COPE_COLOR = [ 0.25 1.0 0.0 ];
+        FACES = [ ...
                 1 2 3 4; ...
                 1 2 6 5; ...
                 1 4 8 5; ...
@@ -70,9 +70,6 @@ classdef (Sealed) BasicMold < handle & PrettyAxes3D
                 3 4 8 7; ...
                 5 6 7 8 ...
                 ];
-            
-        end
-        
     end
     
 end
