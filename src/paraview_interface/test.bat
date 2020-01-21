dc8de160de0ee083c8cd4733ca44f94d40c639c0
wwarriner <wwarriner@gmail.com>
2020-01-21 14:08:15 -0600

    modified test

--- src/paraview_interface/test.bat
+++ src/paraview_interface/test.bat
@@ -1,5 +1,5 @@
 SET script_folder="D:/repos/casting_geometric_toolsuite/src/paraview_interface"
-SET input_folder="C:/Users/wwarr/Desktop/a/bearing_block"
+SET input_folder="C:/Users/wwarr/Desktop/a/base_plate"
 SET name="bearing_block"
 
 START paraview --script="dummy.py"
